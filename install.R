eb2b92ffe35cf6f38ecdcdba72f9b135cfa8efc5
Cam <cameronmcmenamie@gmail.com>
2022-01-20 17:50:33 +1100

    add packages

--- install.R
+++ install.R
@@ -1,1 +1,3 @@
-install.packages(c("tidyverse", "here", "DT", "pheatmap", "BiocManager")) 
+install.packages(c("tidyverse", "here", "DT", "pheatmap", "BiocManager"))
+
+BiocManager::install(c("scater", "SingleCellExperiment", "DropletUtils"))
